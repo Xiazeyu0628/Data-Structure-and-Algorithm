675b6cf2ddea7df2d70d78c69d4041b7c56c4318
Xiazeyu0628 <51402946+Xiazeyu0628@users.noreply.github.com>
2022-01-05 20:37:23 +0800

    update heap

--- README.md
+++ README.md
@@ -9,8 +9,21 @@
 
 
 ## 堆heap
-1. 从列表创建堆
-2. shiftDown
+堆最重要的性质：
+大顶堆：每个节点的值都大于或等于其子节点的值，根节点为全局最大值，在堆排序算法中用于升序排列；
+小顶堆：每个节点的值都小于或等于其子节点的值，根节点为全局最小值，在堆排序算法中用于降序排列；
+
+###自建堆(build_heap)
+**思路**：从列表直接创建堆构建shift_down(node,len)函数，shift_down用于维护以传入的node节点为根节点的heap结构。一方面，我们需要保证每一个节点都下降到不能再下降为止，即最底下的小值不被shift上来最上面的大值就没法完全沉下去，另一方面，每一次shift_down都会可能会破坏上方的堆结构，因此我们需要从后往前遍历节点。由于叶节点没有子节点无需比较，我们只需要从最后一个有子节点的节点反向遍历即可。  
+
+**步骤**:
+1. 找到最后一个要遍历的node
+2. shift_down
+3. 再找上一个node，重复操作直到根节点
+
+
+**时间复杂度O(n)是怎么来的**
+![](../../../../../var/folders/35/0lxq769j30jbkyy97nd62vv40000gn/T/TemporaryItems/（screencaptureui正在存储文稿）/截屏2022-01-05 18.29.24.png)
 
 ### 堆排序
 1. 将待排序序列构建成一个堆 H[0……n-1]，根据（升序降序需求）选择大顶堆或小顶堆；
@@ -22,3 +35,15 @@
 4. 重复步骤 2，直到堆的尺寸为 1。
 
 
+
+## Reference
+自建堆和堆排序部分:  
+
+https://blog.csdn.net/hrn1216/article/details/51465270
+https://www.runoob.com/w3cnote/heap-sort.html
+https://blog.csdn.net/Winnie_boy/article/details/117295050  
+
+算法复杂度部分:  
+
+https://www.zhihu.com/question/20729324
+https://blog.csdn.net/LeoSha/article/details/46116959?spm=1001.2101.3001.6661.1&utm_medium=distribute.pc_relevant_t0.none-task-blog-2%7Edefault%7ECTRLIST%7Edefault-1.no_search_link&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-2%7Edefault%7ECTRLIST%7Edefault-1.no_search_link&utm_relevant_index=1
--- myheap_itr.py
+++ myheap_itr.py
@@ -1,44 +1,65 @@
 inputList = [3,4,2,6,9,0,1,7,5,8]
 
+class myheap(object):
+    def __init__(self,inputList = []):
+        self.list = inputList
+        self.length = len(self.list)
 
-def heapify(arr): #迭代创建小根堆
-    n = len(arr)
-    # 这里//2的考量因素在于遍历所有根茎节点，叶节点因为无法下沉不考虑
-    # eapify函数实际上是对传入的arr[i]点在其所有根部节点中确定它的位置；
-    # 为什么要逆序，因为要把最底下的小值，一点点bubble上来。
-    for i in reversed(range(n // 2)): # [4,3,2,1,0]
-        shiftDown(arr,n,i)
+    def build_heap(self): #迭代创建小根堆
+        # 这里//2的考量因素在于遍历所有根茎节点，叶节点因为无法下沉不考虑
+        # heapify函数实际上是对传入的arr[i]点在其所有根部节点中确定它的位置；
+        # 为什么要逆序，因为要把最底下的小值，一点点bubble上来。
+        for node in reversed(range(self.length // 2)): # [4,3,2,1,0]
+            self.shift_down(self.list, self.length, node)
 
-def shiftDown(arr, n, k):  # 这里参数有n，完全是为了方便写后序的heap_sort
-    # k是根节点的index
-    # j = 2*k + 1 是左子节点的index
-    # j+1 代表的是右子节点的index
-    while 2 * k + 1 < n: # 没有超过堆的元素的个数
-        j = 2 * k + 1
-        if j + 1 < n and arr[j + 1] < arr[j]:  # 如果右子节点小于左子节点
-            j += 1  #那么即将下降交换的是右子节点
+    def swift(self,i,j):
+        self.list[i],self.list[j] = self.list[j],self.list[i]
 
-        if arr[k] <= arr[j]:  #根节点小于最小的子节点
-            break # 不交换
-        else: #根节点大于最小的子节点
-            arr[k], arr[j] = arr[j], arr[k] # 交换
-            k = j #当前根节点来到交换之后的下标，继续判断是否还需要下沉
 
-heapify(inputList)
-print(inputList)
+    def shift_down(self, arr, length, node):  # 这里参数有n，完全是为了方便写后序的heap_sort
+        # k是根节点的index
+        # j = 2*k + 1 是左子节点的index
+        # j+1 代表的是右子节点的index
+
+
+        # 没有超过堆的元素的个数的判断用于
+        # 完全二叉树存在只有node_left没有node_right的情况
+        # 最主要是为了解决堆排序时的堆size逐步缩小的情况
+
+
+        while 2 * node + 1< length:
+            node_left =  2 * node + 1
+            waited_swift_node = node_left
+            node_right = 2 * node + 2
+
+            if node_right < length:
+                if self.list[node_left] > self.list[node_right]:
+                    waited_swift_node = node_right
+
+            if self.list[waited_swift_node]<self.list[node]:
+                self.swift(waited_swift_node,node)
+                node = waited_swift_node
+            else:
+                break
 
 # 堆排序的核心思想在于，最顶上的根节点一定是最小的值，因为它小于它的子节点，又小于它的孙子节点
 # 其他节点尽管因为堆的性质会小于自己的子节点，但是不保证小于同层节点的子节点
 
-def heap_sort(arr):
-    n = len(arr)
-    while n>1: # n~[2,n] arr最小的长度为2，即最少有两个节点
-        # 当 n = 2 时直接跳出来，你动脑子想一下 [8,9.........] -> [9,8........]
-        # 此时shiftDown(arr,1,0)也会直接跳出while循环
-        inputList[0],inputList[n-1] = inputList[n-1],inputList[0] # n-1代表的是末尾index
-        n = n - 1
-        shiftDown(arr,n,0)
+    def heap_sort(self):
+        n = len(self.list)
+        while n>1: # n~[2,n] arr最小的长度为2，即最少有两个节点
+            # 当 n = 2 时直接跳出来，你动脑子想一下 [8,9.........] -> [9,8........]
+            # 此时shiftDown(arr,1,0)也会直接跳出while循环
+            self.swift(0,n-1)
+            n = n - 1
+            self.shift_down(self.list,n,0)
 
-heap_sort(inputList)
-print(inputList)
+heap = myheap(inputList)
+heap.build_heap()
+print(heap.list)
+heap.heap_sort()
+print(heap.list)
+
+
+
 
